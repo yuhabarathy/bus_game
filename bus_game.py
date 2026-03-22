ad07741df0028f8e84b69b160c5f2a6a484634d9
yuhabarathy <yuhabarathyshanmugalingam@gmail.com>
2026-03-22 10:26:45 +0530

    Created complete version of bus game and its function like : add_players, start the game, exit from the game!

--- bus_game.py
+++ bus_game.py
@@ -1,50 +1,40 @@
 player = []
-s_num = 1
 
 def add_player():
-    num = int(input("Enter the total no.of players: "))
+    num = int(input("\nEnter the total no.of players: "))
     for i in range(num):
-        name = input(f"Enter name of player {i+1}: ")
+        name = input(f"\nEnter name of player {i+1}: ")
         player.append(name)
     
-    print("Players added successfully!")
+    print("\nPlayers added successfully!")
     for data in player:
-        print(data, end=" ")
+        print(data)
 
 def start():
-   global s_num
-   if len(player) < 2:
-        print("Add atleast 2 players!")
-        return
+    s_num = 1
+    while(len(player) > 1):
+        for i in range(len(player)):
+            n = input(f"Player {i+1} turn: ").lower()
 
-   while (len(player) > 1):
-    i=0
+            if n == "bus" and s_num % 5 == 0:
+                print("Correct!")
+                s_num += 1
+            
+            elif int(n) == s_num and s_num%5  != 0:
+                print("Correct!")
+                s_num += 1
 
-   while i < len(player):
-        print("="*10 , end=" ")
-        print(f"\n{player[i]}'s turn")
-        print("="*10)
+            else:
+                print("Incorrect!")
+                player.pop(i)
+                s_num += 1
+                if len(player) == 1:
+                    break
+                else:
+                    continue
+    print(f"Congratulations, {player[0]} won!!!")
 
-        ans = input("Enter number: ").lower()
-
-        if ans == "bus" and s_num%5 == 0:
-            print("correct!")
-            s_num += 1
-
-        elif int(ans) == s_num and s_num%5!= 0:
-            print("correct!")
-            s_num += 1
-            i+=1
-
-        else:
-            print(f"{player[i]} wrong answer!")
-            player.pop(i)
-            s_num += 1
-
-            if len(player) == 1:
-                print(f"\nPlayer {player[0]} Won, congratulation!")
-                return
-        
+   
 
 while True:
     try:
@@ -53,7 +43,7 @@ while True:
         print("2.Start the game")
         print("3.Exit")
         print("==========================")
-        
+
         option = int(input("Enter an option number: "))
 
         if option == 1:
@@ -70,4 +60,4 @@ while True:
             print("Invalid option entered. Try again!")
 
     except Exception as e:
-        print("Error: ", e)
+        print("Error: ",e)
